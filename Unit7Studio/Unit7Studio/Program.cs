ba761521d833846a484b4be9abdd1680f0e71ffb
dennis-phillips <dennis_phillips@outlook.com>
2020-06-09 12:52:31 -0400

    intial commit

---
+++ Unit7Studio/Unit7Studio/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Unit7Studio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
